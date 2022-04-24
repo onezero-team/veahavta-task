3efaf8e0baab748aebfa33fcd387375bfa5d2a57
Ben Dan <bendan7@gmail.com>
2022-04-24 17:37:15 +0300

    Added AboutTheClinic section

--- components/home-components/about-the-clinic.tsx
+++ components/home-components/about-the-clinic.tsx
@@ -1,7 +1,31 @@
 import { HomePageType } from '@/lib/interface'
 import React from 'react'
 import { WrapperLarge } from '../wrapper'
+import Image from 'next/image'
+import img from '../../public/assets/images/Rect-about.png'
+import { Button } from '../data-components/button'
 
 export default function AboutTheClinic({ data }: HomePageType) {
-  return <WrapperLarge>About the clinic</WrapperLarge>
+  return (
+    <div className="max-w-screen-lg mx-auto md:py-10 grid md:grid-cols-2 gap-14">
+      <div>
+        <Image
+          src={img}
+          width="702px"
+          height="628px"
+          layout="responsive"
+          alt="about-clinic"
+        />
+      </div>
+      <div className="flex flex-col justify-center text-center  rtl:md:text-right ltr:md:text-left">
+        <div className="text-purple text-2xl">{data.homepage.aboutHeading}</div>
+        <div className="font-bold text-5xl">{data.homepage.aboutTitle}</div>
+        <p className="mt-5">{data.homepage.aboutUsText}</p>
+        <Button
+          className="mt-5 max-w-[185px] rounded-full p-2 bg-purple text-light px-5 font-medium self-center md:self-start"
+          text={data.homepage?.getToKnowUsButton}
+        />
+      </div>
+    </div>
+  )
 }
