c3bc6569cbd3f67ab94300e6bb55490234ac1897
Amit Bar-Gil <amit217@yandex.com>
2022-07-29 13:10:32 +0300

    About section

--- components/home-components/about-the-clinic.tsx
+++ components/home-components/about-the-clinic.tsx
@@ -8,8 +8,13 @@ export default function AboutTheClinic({ data }: HomePageType) {
   return (
     <WrapperLarge>
       <div>
-        <div className="about__text" id="about-us">
-          <h2>{data.homepage.aboutHeading}</h2>
+        <div className="about-text" id="about-us">
+          <h2 className="about__heading">{data.homepage.aboutHeading}</h2>
+          <h3 className="about__title">{data.homepage.aboutTitle}</h3>
+          <p className="about__text">{data.homepage.aboutUsText}</p>
+          <button className="about__button" type="button">
+            {data.homepage.aboutUsButton}
+          </button>
         </div>
         <div className="about__image">
           <Image src={aboutLogo} />
@@ -18,13 +23,46 @@ export default function AboutTheClinic({ data }: HomePageType) {
           div {
             display: flex;
             flex-direction: row-reverse;
+            column-gap: 3%;
             justify-content: center;
             margin-top: 3%;
+          }
+          div.about-text {
+            margin-top: 5%;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            overflow: visible;
+            max-width: 530px;
           }
           div.about__image {
             max-width: 700px;
             max-height: 630px;
           }
+          div .about__heading {
+            color: #4e47f9;
+            font-weight: 400;
+            font-size: 28px;
+          }
+          div .about__title {
+            font-weight: 700;
+            font-size: 52px;
+            line-height: 115%;
+          }
+          div .about__text {
+            font-weight: 400;
+            font-size: 18px;
+          }
+          div .about__button {
+            margin-top: 5%;
+            align-self: flex-start;
+            background: #4e47f9;
+            border-radius: 51px;
+            width: 185px;
+            height: 60px;
+            font-weight: 700;
+            font-size: 22px;
+            color: #FFFFFF;
         `}</style>
       </div>
     </WrapperLarge>
