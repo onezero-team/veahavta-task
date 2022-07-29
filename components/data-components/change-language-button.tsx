da2b35322cefa5703f56bf4a71a223488bf12e83
Amit Bar-Gil <amit217@yandex.com>
2022-07-29 10:16:37 +0300

    Build navbar

--- components/data-components/change-language-button.tsx
+++ components/data-components/change-language-button.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 export const ChangeLangButton = ({
   className,
   children,
   lang,
+  imageProp,
 }: {
   className?: string
   children?: React.ReactNode
   lang: string
+  imageProp: any
 }) => {
   const router = useRouter()
   const changeLocale = (lang: string) => {
@@ -17,7 +20,9 @@ export const ChangeLangButton = ({
   }
   return (
     <div>
-      <button onClick={() => changeLocale(lang)}>{lang}</button>
+      <button onClick={() => changeLocale(lang)}>
+        <Image src={imageProp} />
+      </button>
     </div>
   )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,14 @@
 import { useLocale } from '@/lib/hooks'
 import { CommonType } from '@/lib/interface'
-import Link from 'next/link'
 import { ChangeLangButton } from './data-components/change-language-button'
 import OneZeroSkipToMainContent from './onezero-skip-to-main-content'
 import Logo from './modular-components/Logo'
+import HeaderLinks from './modular-components/HeaderLinks'
+import israelLogo from '../public/assets/israel-lan.svg'
+import englishLogo from '../public/assets/english-lan.svg'
+import tanzaniaLogo from '../public/assets/tanzania-lan.svg'
+import arabicLogo from '../public/assets/arabic-lan.svg'
+import amharicLogo from '../public/assets/amharic-lan.svg'
 
 export default function Header({ data }: CommonType) {
   const { dir } = useLocale()
@@ -17,18 +22,47 @@ export default function Header({ data }: CommonType) {
       <header className="h-header z-10 pt-4 px-4">
         <div className="grid grid-cols-auto-1fr gap-x-6 mx-auto max-w-screen-lg p-3 bg-light rounded-lg">
           <Logo />
-
-          <ul className="flex flex-row gap-x-2">
-            <li>
-              <ChangeLangButton className="" lang="en">
-                {data.languageNames[0].en}
-              </ChangeLangButton>
-            </li>
-            <li>
-              <ChangeLangButton className="" lang="he">
-                {data.languageNames[0].he}
-              </ChangeLangButton>
-            </li>
+          <ul className="flex flex-row gap-x-2 font-bold items-center justify-between">
+            <HeaderLinks headerData={data.appLinks} />
+            <div className="flex flex-row gap-x-2 font-bold items-center mr-auto">
+              <li>
+                <ChangeLangButton className="" lang="ar" imageProp={arabicLogo}>
+                  {data.languageNames[0].ar}
+                </ChangeLangButton>
+              </li>
+              <li>
+                <ChangeLangButton className="" lang="he" imageProp={israelLogo}>
+                  {data.languageNames[0].he}
+                </ChangeLangButton>
+              </li>
+              <li>
+                <ChangeLangButton
+                  className=""
+                  lang="en"
+                  imageProp={englishLogo}
+                >
+                  {data.languageNames[0].en}
+                </ChangeLangButton>
+              </li>
+              <li>
+                <ChangeLangButton
+                  className=""
+                  lang="am"
+                  imageProp={amharicLogo}
+                >
+                  {data.languageNames[0].am}
+                </ChangeLangButton>
+              </li>
+              <li>
+                <ChangeLangButton
+                  className=""
+                  lang="ti"
+                  imageProp={tanzaniaLogo}
+                >
+                  {data.languageNames[0].ti}
+                </ChangeLangButton>
+              </li>
+            </div>
           </ul>
         </div>
       </header>
