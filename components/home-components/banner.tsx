0322ce2906aa5721f978e9bb56a00615cca3c3a3
Amit Bar-Gil <amit217@yandex.com>
2022-07-30 11:28:04 +0300

    Banner z-index fix

--- components/home-components/banner.tsx
+++ components/home-components/banner.tsx
@@ -2,20 +2,23 @@ import { HomePageType } from '@/lib/interface'
 import themePreval from '@/lib/theme.preval'
 import React from 'react'
 import { PageHeader } from '../data-components/header-text'
-import groupImage from '../../public/assets/group.png'
 import Image from 'next/image'
+import groupImage from '../../public/assets/group.png'
 
 export default function Banner({ data }: HomePageType) {
   return (
     <div
       id="top-header"
-      className="overflow-hidden relative mobile:max-w-full  "
+      className="overflow-hidden relative mobile:max-w-full "
     >
-      {/* <div className="absolute -bottom-36 mobile:hidden ">
+      <div className="absolute  -top-36  left-1 mobile:hidden  ">
         <Image src={groupImage} />
-      </div> */}
-      <div className="wrapper pb-6 bg-banner  mobile:p-10 mobile:pb-36">
-        <PageHeader>{data.homepage}</PageHeader>
+      </div>
+      <div className="wrapper pb-6 bg-banner  mobile:p-10 mobile:pb-36 ">
+        <div className=" z-50">
+          <PageHeader>{data.homepage}</PageHeader>
+        </div>
+
         <button className="button text-2xl  w-48 self-center h-16 bg-icon-bg  text-light rounded-full mt-5  mobile:mt-10">
           {data.homepage.getToKnowUsButton}
         </button>
--- components/main.tsx
+++ components/main.tsx
@@ -3,12 +3,7 @@ import React from 'react'
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main
-        id="main-content"
-        className=" max-w-full m-auto flex flex-col justify-items-center box-border"
-      >
-        {children}
-      </main>
+      <main id="main-content ">{children}</main>
     </>
   )
 }
