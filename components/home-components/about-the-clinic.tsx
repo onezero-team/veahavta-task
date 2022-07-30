c0d1a551a07ee9ae32c3240f50ec65c668b0b3ba
Amit Bar-Gil <amit217@yandex.com>
2022-07-30 08:59:49 +0300

    About us 425px style

--- components/home-components/about-the-clinic.tsx
+++ components/home-components/about-the-clinic.tsx
@@ -7,63 +7,28 @@ import aboutLogo from '../../public/assets/about-hands.png'
 export default function AboutTheClinic({ data }: HomePageType) {
   return (
     <WrapperLarge>
-      <div>
-        <div className="about-text" id="about-us">
-          <h2 className="about__heading">{data.homepage.aboutHeading}</h2>
-          <h3 className="about__title">{data.homepage.aboutTitle}</h3>
-          <p className="about__text">{data.homepage.aboutUsText}</p>
-          <button className="about__button" type="button">
+      <div className="flex flex-row-reverse gap-x-20 justify-center mt-36 mobile:flex-col-reverse mobile:gap-y-5 mobile:mb-28">
+        <div
+          className=" mt-10 flex flex-col justify-start  max-w-xl mobile:flex mobile:flex-col mobile:gap-y-5 "
+          id="about-us"
+        >
+          <h2 className=" text-icon-bg text-3xl ">
+            {data.homepage.aboutHeading}
+          </h2>
+          <h3 className="font-bold text-6xl mobile:text-4xl">
+            {data.homepage.aboutTitle}
+          </h3>
+          <p className=" text-lg">{data.homepage.aboutUsText}</p>
+          <button
+            className=" self-start bg-icon-bg rounded-full w-48 h-16 text-contact-bg font-bold text-xl mt-4"
+            type="button"
+          >
             {data.homepage.aboutUsButton}
           </button>
         </div>
-        <div className="about__image">
+        <div className="max-w-2xl">
           <Image src={aboutLogo} />
         </div>
-        <style jsx>{`
-          div {
-            display: flex;
-            flex-direction: row-reverse;
-            column-gap: 3%;
-            justify-content: center;
-            margin-top: 3%;
-          }
-          .about-text {
-            margin-top: 5%;
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
-            overflow: visible;
-            max-width: 530px;
-          }
-          .about__image {
-            max-width: 700px;
-            max-height: 630px;
-          }
-           .about__heading {
-            color: #4e47f9;
-            font-weight: 400;
-            font-size: 28px;
-          }
-           .about__title {
-            font-weight: 700;
-            font-size: 52px;
-            line-height: 115%;
-          }
-           .about__text {
-            font-weight: 400;
-            font-size: 18px;
-          }
-           .about__button {
-            margin-top: 5%;
-            align-self: flex-start;
-            background: #4e47f9;
-            border-radius: 51px;
-            width: 185px;
-            height: 60px;
-            font-weight: 700;
-            font-size: 22px;
-            color: #FFFFFF;
-        `}</style>
       </div>
     </WrapperLarge>
   )
