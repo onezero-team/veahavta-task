b9da9fab38798ec85ec19a6b5ecff9c814d366bc
YohayHackam <yoman321@gmail.com>
2022-07-31 20:03:16 +0300

    fix what we do paging

--- components/home-components/what-we-do.tsx
+++ components/home-components/what-we-do.tsx
@@ -16,20 +16,30 @@ export default function WhatWeDo({ data }: HomePageType) {
     side: 'left' | 'right',
   ) => {
     if (side === 'left') {
-      element.scrollLeft -= (element.clientWidth + 20)
+      element.scrollLeft -= element.clientWidth + 20
       setRightStop(false)
-      
-      if ((dir === 'ltr' && element.clientWidth + 21 > element.scrollLeft ) || (dir === 'rtl' && element.scrollWidth - element.clientWidth < element.clientWidth+21  +  Math.abs(element.scrollLeft)))
+
+      if (
+        (dir === 'ltr' && element.clientWidth + 21 > element.scrollLeft) ||
+        (dir === 'rtl' &&
+          element.scrollWidth - element.clientWidth <
+            element.clientWidth + 21 + Math.abs(element.scrollLeft))
+      )
         setLeftStop(true)
     }
     if (side === 'right') {
-      element.scrollLeft += (element.clientWidth + 20)    
+      element.scrollLeft += element.clientWidth + 20
       setLeftStop(false)
-    
-    if ((dir === 'ltr' && element.scrollLeft + 2*element.clientWidth +21 > element.scrollWidth ) || (dir === 'rtl' &&  Math.abs(element.scrollLeft) < element.clientWidth +21))
-         setRightStop(true)
-    }
 
+      if (
+        (dir === 'ltr' &&
+          element.scrollLeft + 2 * element.clientWidth + 21 >
+            element.scrollWidth) ||
+        (dir === 'rtl' &&
+          Math.abs(element.scrollLeft) < element.clientWidth + 21)
+      )
+        setRightStop(true)
+    }
   }
 
   return (
