49dff96ad40f8c8b6e371105ac6eb8ecc8e07976
Gal <galgross24@gmail.com>
2022-08-22 23:48:42 +0300

    דשגכ

--- components/layout.tsx
+++ components/layout.tsx
@@ -51,7 +51,7 @@ export default function Layout({ children, pageProps }: any) {
 
           *:focus-visible {
             outline: none !important;
-            box-shadow: 0 0 0 2px ${theme.colors.primary} !important;
+            box-shadow: 0 0 0 2px ${theme?.colors?.primary} !important;
             border-radius: ${theme.borderRadius['2xl']};
           }
 
